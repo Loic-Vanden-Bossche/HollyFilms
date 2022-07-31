862f964e2386ed74053af9b5c40b479916b93093
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2022-07-31 18:01:25 +0200

    Added duration & release date

--- packages/frontend/src/app/pages/home/media-modal/media-modal.component.ts
+++ packages/frontend/src/app/pages/home/media-modal/media-modal.component.ts
@@ -1,7 +1,7 @@
 import {
   Component,
   EventEmitter,
-  Input,
+  Input, OnChanges,
   OnInit,
   Output,
   ViewChild,
@@ -11,11 +11,16 @@ import { ModalService } from '../../../shared/services/modal.service';
 import { YouTubePlayer } from '@angular/youtube-player';
 import { faPlusCircle, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
+import * as dayjs from 'dayjs';
+import * as duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
+
 @Component({
   selector: 'app-media-modal',
   templateUrl: './media-modal.component.html',
 })
-export class MediaModalComponent implements OnInit {
+export class MediaModalComponent implements OnChanges , OnInit {
   @Input() media: MediaWithType | null = null;
   @Output() closed: EventEmitter<void> = new EventEmitter<void>();
 
@@ -28,6 +33,8 @@ export class MediaModalComponent implements OnInit {
     iv_load_policy: 3,
     mute: 1,
   };
+
+  displayedTime: string = '';
 
   seekTime = 20;
 
@@ -47,6 +54,11 @@ export class MediaModalComponent implements OnInit {
       document.body.appendChild(tag);
       this.apiLoaded = true;
     }
+  }
+
+  ngOnChanges() {
+    const time = 128;
+    this.displayedTime = dayjs.duration(time, 'minute').format('H[h]mm').replace(/00$/, '');
   }
 
   onVideoReady() {
