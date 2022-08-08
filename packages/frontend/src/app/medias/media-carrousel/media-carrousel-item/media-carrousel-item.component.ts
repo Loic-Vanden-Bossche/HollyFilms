4c64fbfaa7e0de58bff7f40d1646b9de305ed55e
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2022-08-08 03:45:18 +0200

    Finished intial carousel

--- packages/frontend/src/app/medias/media-carrousel/media-carrousel-item/media-carrousel-item.component.ts
+++ packages/frontend/src/app/medias/media-carrousel/media-carrousel-item/media-carrousel-item.component.ts
@@ -1,12 +1,105 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MediaWithTypeAndFeatured } from '../../../shared/models/media.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  FeaturedType,
+  MediaWithTypeAndFeatured,
+} from '../../../shared/models/media.model';
+import {
+  faArrowTrendUp,
+  faBolt,
+  faCirclePlay,
+  faList,
+  faPlusCircle,
+  faThumbsUp,
+  faUserCheck,
+} from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/free-regular-svg-icons';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { PlayerService } from '../../../shared/services/player.service';
 
 @Component({
   selector: 'app-media-carrousel-item',
   templateUrl: './media-carrousel-item.component.html',
+  animations: [
+    trigger('onCardOpenClose', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'TranslateX(-20px) Scale(0.9)',
+        }),
+        animate(
+          '1s ease',
+          style({
+            opacity: 1,
+            transform: 'TranslateX(0px) Scale(1)',
+          })
+        ),
+      ]),
+      transition(':leave', [
+        style({
+          opacity: 1,
+          transform: 'TranslateX(0px) Scale(1)',
+        }),
+        animate(
+          '1s ease',
+          style({
+            opacity: 0,
+            transform: 'TranslateX(-20px) Scale(0.9)',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class MediaCarrouselItemComponent implements OnInit {
   @Input() media: MediaWithTypeAndFeatured | null = null;
+  @Input() selected = false;
 
-  ngOnInit(): void {}
+  @Output() mediaSelected = new EventEmitter<void>();
+
+  vendorTag = '';
+  vendorIcon: IconDefinition | null = null;
+
+  addToListIcon = faPlusCircle;
+  likeButton = faThumbsUp;
+
+  ratingProgress = 0;
+
+  constructor(private readonly playerService: PlayerService) {}
+
+  getTagAndIconFromFeatured(featured: FeaturedType): [string, IconDefinition] {
+    switch (featured) {
+      case FeaturedType.CONTINUE:
+        return [
+          'Vous avez commencé ce film, continuer la lecture ?',
+          faCirclePlay,
+        ];
+      case FeaturedType.INLIST:
+        return ['Ceci est dans votre liste', faList];
+      case FeaturedType.POPULAR:
+        return ['Très populaire sur HollyFilms', faArrowTrendUp];
+      case FeaturedType.RECENT:
+        return ['Nouveau sur HollyFilms', faBolt];
+      case FeaturedType.RECOMMENDED:
+        return [' Recommandé pour vous', faUserCheck];
+    }
+  }
+
+  onPlay(event: MouseEvent) {
+    if (this.media) {
+      this.playerService.play({
+        mediaId: this.media.data._id,
+        x: event.clientX,
+        y: event.clientY,
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    if (this.media) {
+      [this.vendorTag, this.vendorIcon] = this.getTagAndIconFromFeatured(
+        this.media.featured
+      );
+      this.ratingProgress = (this.media.data.rating * 100) / 10;
+    }
+  }
 }
--- packages/frontend/src/app/medias/media-carrousel/media-carrousel.component.ts
+++ packages/frontend/src/app/medias/media-carrousel/media-carrousel.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { MediaWithTypeAndFeatured } from '../../shared/models/media.model';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  MediaWithType,
+  MediaWithTypeAndFeatured,
+} from '../../shared/models/media.model';
 import { MediasService } from '../../shared/services/medias.service';
-import { BehaviorSubject } from 'rxjs';
 import { shuffle } from '../../shared/utils';
 
 import EffectCarousel from './carrousel-effect';
@@ -14,6 +16,8 @@ import SwiperCore, {
   SwiperOptions,
   Virtual,
 } from 'swiper';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Swiper } from 'swiper';
 
 SwiperCore.use([
   Navigation,
@@ -27,8 +31,42 @@ SwiperCore.use([
 @Component({
   selector: 'app-media-carrousel',
   templateUrl: './media-carrousel.component.html',
+  animations: [
+    trigger('onCarousel', [
+      transition(
+        ':enter',
+        [
+          style({
+            transform: 'TranslateX(-20px) TranslateY(-150%)',
+          }),
+          animate(
+            '1s {{delay}}ms ease',
+            style({
+              transform: 'TranslateX(0px) TranslateY(0px)',
+            })
+          ),
+        ],
+        { params: { delay: 0 } }
+      ),
+    ]),
+    trigger('onCarouselGlobal', [
+      transition(':enter', [
+        style({
+          transform: 'perspective(500px) scaleZ(2) rotateY(10deg)',
+        }),
+        animate(
+          '3s ease',
+          style({
+            transform: 'perspective(1) scaleZ(1) rotateY(0deg)',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class MediaCarrouselComponent implements OnInit {
+  @Output() mediaSelected = new EventEmitter<MediaWithType>();
+
   featured: MediaWithTypeAndFeatured[] = [];
   index = 0;
 
@@ -47,18 +85,23 @@ export class MediaCarrouselComponent implements OnInit {
       el: '.swiper-pagination',
     },
     autoplay: {
-      delay: 3000,
+      delay: 6000,
     },
   };
 
-  displayedMedias = new BehaviorSubject<MediaWithTypeAndFeatured[]>([]);
-
   constructor(private readonly mediasService: MediasService) {}
+
+  slideChanged([swiper]: Swiper[]) {
+    if (swiper.isBeginning) {
+      this.index = 0;
+    } else {
+      this.index = swiper.realIndex;
+    }
+  }
 
   ngOnInit(): void {
     this.mediasService.getFeaturedMedias().subscribe((medias) => {
       this.featured = shuffle(medias);
-      console.log(this.featured.length);
     });
   }
 }
