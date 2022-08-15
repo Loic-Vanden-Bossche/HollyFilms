f9fc606528670ab23221c68a7929e8cc8bc7f77f
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2022-08-15 23:17:33 +0200

    Added responsive behaviors for cards

--- packages/frontend/src/app/medias/media-card/media-card-data/media-card-data.component.ts
+++ packages/frontend/src/app/medias/media-card/media-card-data/media-card-data.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MediaWithType } from '../../../shared/models/media.model';
 import { faPlusCircle, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { MediasService } from '../../../shared/services/medias.service';
+import { ModalService } from '../../../shared/services/modal.service';
 
 @Component({
   selector: 'app-media-card-data',
@@ -12,4 +14,22 @@ export class MediaCardDataComponent {
 
   addToListIcon = faPlusCircle;
   likeButton = faThumbsUp;
+
+  constructor(
+    private readonly mediasService: MediasService,
+    private readonly modalService: ModalService
+  ) {}
+
+  onMouseOnDetails(): void {
+    if (window.screen.width > 768) {
+      this.showMore.emit();
+    }
+  }
+
+  onDetailsClick(): void {
+    if (this.media) {
+      this.mediasService.selectMedia(this.media);
+      this.modalService.open('mediaModal');
+    }
+  }
 }
