cee246c13fae1989e5c50611ec3cd4813f4c7ce8
Loïc Vanden Bossche <l.vandenbossche.jouvet@gmail.com>
2022-08-16 03:09:34 +0200

    Added repsonsive design to modal

--- packages/frontend/src/app/medias/actor-list/actor-list.component.ts
+++ packages/frontend/src/app/medias/actor-list/actor-list.component.ts
@@ -1,12 +1,61 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Actor } from '../../shared/models/actor.model';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-actor-list',
   templateUrl: './actor-list.component.html',
+  animations: [
+    trigger('onControl', [
+      transition(':enter', [
+        style({
+          transform: 'TranslateX({{transform}}%)',
+        }),
+        animate(
+          '0.5s ease',
+          style({
+            transform: 'TranslateX(0%)',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ActorListComponent {
   @Input() actors: Actor[] = [];
-
   @ViewChild('container') container: ElementRef | null = null;
+
+  scroll = 800;
+
+  chevronRightIcon = faChevronRight;
+  chevronLeftIcon = faChevronLeft;
+
+  get canScrollLeft() {
+    return this.container && this.container.nativeElement.scrollLeft > 0;
+  }
+
+  get canScrollRight() {
+    return (
+      this.container &&
+      this.container.nativeElement.scrollLeft +
+        this.container.nativeElement.getBoundingClientRect().width <
+        this.container.nativeElement.scrollWidth
+    );
+  }
+
+  onScrollLeft() {
+    if (this.container) {
+      this.container.nativeElement.scrollLeft -= this.scroll;
+    }
+  }
+
+  onScrollRight() {
+    if (this.container) {
+      this.container.nativeElement.scrollLeft += this.scroll;
+    }
+  }
 }
